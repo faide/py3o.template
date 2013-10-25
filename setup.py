2b9a7b1dfe2d4e2995ffa21e2176325a9b8f8cf2
Florent Aide <florent.aide@gmail.com>
2013-10-25 16:35:48 +0200

    pep8 cleanup of setup.py file

--- setup.py
+++ setup.py
@@ -1,39 +1,38 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.4'
 
-setup(name='py3o.template',
-      version=version,
-      description="An easy solution to design reports using OpenOffice",
-      long_description=open("README.txt").read(),
-      classifiers=[
+setup(
+    name='py3o.template',
+    version=version,
+    description="An easy solution to design reports using OpenOffice",
+    long_description=open("README.txt").read(),
+    classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-      ],
-      keywords='LibreOffice OpenOffice templating PDF',
-      author='Florent Aide',
-      author_email='florent.aide@gmail.com',
-      url='http://bitbucket.org/faide/py3o.template',
-      license='BSD License',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      namespace_packages=['py3o'],
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          'setuptools',
-          'lxml',
-          'genshi',
-          'pyjon.utils',
-          ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      tests_require=['nose', 'nosexcover'],
-      test_suite = 'nose.collector',
-      )
-2
+    ],
+    keywords='LibreOffice OpenOffice templating PDF',
+    author='Florent Aide',
+    author_email='florent.aide@gmail.com',
+    url='http://bitbucket.org/faide/py3o.template',
+    license='BSD License',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['py3o'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'lxml',
+        'genshi',
+        'pyjon.utils',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    tests_require=['nose', 'nosexcover'],
+    test_suite='nose.collector',
+)
