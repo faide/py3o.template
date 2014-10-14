d021350f2a65c4992d2b0308c2f0af30079e4568
Florent Aide <florent.aide@gmail.com>
2014-10-14 12:16:19 +0200

    Added tests for the new get_user_variables method. Fixed test with python3

--- py3o/template/tests/test_helpers.py
+++ py3o/template/tests/test_helpers.py
@@ -2,9 +2,11 @@ __author__ = 'faide'
 
 import unittest
 from py3o.template.main import move_siblings
+from py3o.template.main import Template
 import lxml.etree
 import pkg_resources
 from io import BytesIO
+from pyjon.utils import get_secure_filename
 
 
 class TestMoveSiblings(unittest.TestCase):
@@ -21,9 +23,7 @@ class TestMoveSiblings(unittest.TestCase):
             'py3o.template',
             'tests/templates/move_one.xml'
         )
-        test_template_one = lxml.etree.parse(
-            BytesIO(open(template_one_name).read())
-        )
+        test_template_one = lxml.etree.parse(template_one_name)
         start = test_template_one.find('mystruct/start')
         end = test_template_one.find('mystruct/end')
         new_ = test_template_one.find('mystruct/finishcontainer')
@@ -33,7 +33,7 @@ class TestMoveSiblings(unittest.TestCase):
         result_s = lxml.etree.tostring(
             test_template_one,
             pretty_print=True,
-        )
+        ).decode('utf-8')
 
         expected_result = open(
             pkg_resources.resource_filename(
@@ -41,6 +41,11 @@ class TestMoveSiblings(unittest.TestCase):
                 'tests/templates/move_one_result.xml'
             )
         ).read()
+
+        expected_result = expected_result.replace(
+            '\n', '').replace(' ', '').strip()
+
+        result_s = result_s.replace('\n', '').replace(' ', '').strip()
 
         assert result_s == expected_result
 
@@ -50,9 +55,7 @@ class TestMoveSiblings(unittest.TestCase):
             'py3o.template',
             'tests/templates/move_two.xml'
         )
-        test_template_two = lxml.etree.parse(
-            BytesIO(open(template_two_name).read())
-        )
+        test_template_two = lxml.etree.parse(template_two_name)
         start = test_template_two.find('mystruct/start')
         end = test_template_two.find('mystruct/end')
         new_ = test_template_two.find('mystruct/finishcontainer')
@@ -62,7 +65,9 @@ class TestMoveSiblings(unittest.TestCase):
         result_s = lxml.etree.tostring(
             test_template_two,
             pretty_print=True,
-        )
+        ).decode('utf-8')
+
+        print(type(result_s))
 
         expected_result = open(
             pkg_resources.resource_filename(
@@ -71,6 +76,33 @@ class TestMoveSiblings(unittest.TestCase):
             )
         ).read()
 
+        expected_result = expected_result.replace(
+            '\n', '').replace(' ', '').strip()
+
+        result_s = result_s.replace('\n', '').replace(' ', '').strip()
+
         print(result_s)
 
         assert result_s == expected_result
+
+    def test_get_user_variables(self):
+        source_odt_filename = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/py3o_example_template.odt'
+        )
+        outfilename = get_secure_filename()
+
+        template = Template(source_odt_filename, outfilename)
+
+        vars = template.get_user_variables()
+
+        expected_vars = [
+            'py3o.line.val1',
+            'py3o.line.val2',
+            'py3o.line.val3',
+            'py3o.item.Amount',
+            'py3o.item.Currency',
+            'py3o.item.InvoiceRef',
+            'py3o.document.total',
+        ]
+        assert set(vars) == set(expected_vars)
