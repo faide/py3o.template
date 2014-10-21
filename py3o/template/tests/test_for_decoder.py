4c44f6223b1814273749cf512cdfdd307d48366a
Anael Lorimier <anael.lorimier@xcg-consulting.fr>
2014-10-21 17:28:31 +0200

    Add test suite for the parser

---
+++ py3o/template/tests/test_for_decoder.py
@@ -0,0 +1,61 @@
+__author__ = 'alorimier'
+
+import unittest
+from py3o.template.decoder import Decoder
+
+
+class TestDecoder(unittest.TestCase):
+    def setUp(self):
+        self.d = Decoder()
+
+    def test_for_implemented(self):
+        """test if exception is raised correctly"""
+        error = False
+        try:
+            self.d.decode("for i in my_list: pass\n")
+        except NotImplementedError:
+            error = True
+        assert error is False
+
+    def test_simple_for_variables(self):
+        """ Test a simple for loop vars
+        """
+        self.d.decode("for i in toto: pass\n")
+        v = self.d.get_variables()
+        assert v == 'i'
+
+    def test_tuple_for_variable(self):
+        """ Test when a tuple is defined in the for loop
+        """
+        self.d.decode("for i, j in toto: pass\n")
+        v = self.d.get_variables()
+        assert v == ['i', 'j']
+
+    def test_simple_for_iter(self):
+        """ Test a simple for loop iter
+        """
+        self.d.decode("for i in toto: pass\n")
+        its = self.d.get_iterables()
+        assert its == 'toto'
+
+    def test_simple_callable_for_iter(self):
+        """ Test when iter is a callable
+        """
+        self.d.decode("for i, j in enumerate(list): pass\n")
+        its = self.d.get_iterables()
+        assert its.get_func_str() == 'enumerate(list)'
+
+    def test_callable_w_kwargs_for_iter(self):
+        """ Test when iter is a callable with keywords arguments
+        """
+        self.d.decode("for i, j in func(arg1, kwarg1=some_values): pass\n")
+        its = self.d.get_iterables()
+        assert its.get_func_str() == 'func(arg1, kwarg1=some_values)'
+
+    #def test_for_dot_attribute(self):
+    #    """ Test when iter is a callable from a dotted expression
+    #    """
+    #    self.d.decode("for i in test.myattr: pass\n")
+    #    its = self.d.get_iterables()
+    #    assert its == 'test.myattr'
+
